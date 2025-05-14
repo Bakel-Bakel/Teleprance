778cf582f2ae79aaecf87bdec8281e0ea32b7a19
Bakel Bakel <begededum4bakel@gmail.com>
2025-05-14 03:32:44 +0200

    added motorSpeed

--- arduino-code/tests/motion-test/motion-test.ino
+++ arduino-code/tests/motion-test/motion-test.ino
@@ -25,49 +25,57 @@ void setup() {
 }
 
 void loop() {
-  if (Serial.available()) {
-    char command = Serial.read();  // Read incoming data from Serial Monitor
-
-    // Check for the command received and control motors accordingly
-    if (command == 'F') {
-      // Move Forward
-      Serial.println("Moving Forward");
-      digitalWrite(motorPin1, HIGH);
-      digitalWrite(motorPin2, LOW);
-      digitalWrite(motorPin3, HIGH);
-      digitalWrite(motorPin4, LOW);
-    } 
-    else if (command == 'B') {
-      // Move Backward
-      Serial.println("Moving Backward");
-      digitalWrite(motorPin1, LOW);
-      digitalWrite(motorPin2, HIGH);
-      digitalWrite(motorPin3, LOW);
-      digitalWrite(motorPin4, HIGH);
-    } 
-    else if (command == 'L') {
-      // Turn Left
-      Serial.println("Turning Left");
-      digitalWrite(motorPin1, HIGH);
-      digitalWrite(motorPin2, LOW);
-      digitalWrite(motorPin3, LOW);
-      digitalWrite(motorPin4, HIGH);
-    } 
-    else if (command == 'R') {
-      // Turn Right
-      Serial.println("Turning Right");
-      digitalWrite(motorPin1, LOW);
-      digitalWrite(motorPin2, HIGH);
-      digitalWrite(motorPin3, HIGH);
-      digitalWrite(motorPin4, LOW);
-    } 
-    else if (command == 'S') {
-      // Stop Motors
-      Serial.println("Stopping");
-      digitalWrite(motorPin1, LOW);
-      digitalWrite(motorPin2, LOW);
-      digitalWrite(motorPin3, LOW);
-      digitalWrite(motorPin4, LOW);
+    if (Serial.available()) {
+      char command = Serial.read();  // Read incoming data from Serial Monitor
+  
+      // Default speed, you can modify this as needed
+      int motorSpeed = 255;  // Full speed (0 to 255, where 255 is max speed)
+      
+      // Check for the command received and control motors accordingly
+      if (command == 'F') {
+        // Move Forward
+        Serial.println("Moving Forward");
+        digitalWrite(motorPin1, HIGH);
+        digitalWrite(motorPin2, LOW);
+        digitalWrite(motorPin3, HIGH);
+        digitalWrite(motorPin4, LOW);
+        analogWrite(enablePin, motorSpeed);  // Set motor speed (PWM)
+      } 
+      else if (command == 'B') {
+        // Move Backward
+        Serial.println("Moving Backward");
+        digitalWrite(motorPin1, LOW);
+        digitalWrite(motorPin2, HIGH);
+        digitalWrite(motorPin3, LOW);
+        digitalWrite(motorPin4, HIGH);
+        analogWrite(enablePin, motorSpeed);  // Set motor speed (PWM)
+      } 
+      else if (command == 'L') {
+        // Turn Left
+        Serial.println("Turning Left");
+        digitalWrite(motorPin1, HIGH);
+        digitalWrite(motorPin2, LOW);
+        digitalWrite(motorPin3, LOW);
+        digitalWrite(motorPin4, HIGH);
+        analogWrite(enablePin, motorSpeed);  // Set motor speed (PWM)
+      } 
+      else if (command == 'R') {
+        // Turn Right
+        Serial.println("Turning Right");
+        digitalWrite(motorPin1, LOW);
+        digitalWrite(motorPin2, HIGH);
+        digitalWrite(motorPin3, HIGH);
+        digitalWrite(motorPin4, LOW);
+        analogWrite(enablePin, motorSpeed);  // Set motor speed (PWM)
+      } 
+      else if (command == 'S') {
+        // Stop Motors
+        Serial.println("Stopping");
+        digitalWrite(motorPin1, LOW);
+        digitalWrite(motorPin2, LOW);
+        digitalWrite(motorPin3, LOW);
+        digitalWrite(motorPin4, LOW);
+        analogWrite(enablePin, 0);  // Stop the motors by sending 0 to PWM
+      }
     }
   }
-}
